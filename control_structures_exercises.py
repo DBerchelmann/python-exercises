554c4f49f2c69a15999e4fb52640bfa97730711f
David Berchelmann <david.a.berchelmann@gmail.com>
2021-01-13 12:32:15 -0600

    added exercises through 2b

--- control_structures_exercises.py
+++ control_structures_exercises.py
@@ -52,9 +52,76 @@ else:
      # Create a while loop that runs so long as i is less than or equal to 15
      # Each loop iteration, output the current value of i, then increment i by one.
 
+i = 5
+while i <= 15:
+    print(i)
+    i += 1
+
+
+# Create a while loop that will count by 2's starting with 0 and ending at 100. Follow each number with a new line.
+
+i = 0
+while i <= 100:
+    print(i)
+    i += 2
+
+# Alter your loop to count backwards by 5's from 100 to -10.
+
+i = 100
+while i >= -10:
+    print(i)
+    i -= 5
+
+# Create a while loop that starts at 2, and displays the number squared on each line while the number is less than 1,000,000. Output should equal:
+
+i = 2
+while i < 1_000_000:
+    print(i)
+    i = i ** 2
+
+# Write a loop that uses print to create the output shown below.
+
+i = 100
+while i >= 5:
+    print(i)
+    i -= 5
+
+
+# For Loops
+
+# Write some code that prompts the user for a number, then shows a multiplication table up through 10 for that number.
+
+user_number = int(input('please enter your favorite number? '))
 
 
 
+for n in range(1, 11):
+    print(n, "x", user_number, "=", user_number*n)
 
+# Create a for loop that uses print to create the output shown below.
 
-    
+for n in range(10):
+    print(str((n))*n)
+
+# break and continue
+
+# Prompt the user for an odd number between 1 and 50. Use a loop and a break statement to continue prompting the user if they enter invalid input. 
+# (Hint: use the isdigit method on strings to determine this).
+ # Use a loop and the continue statement to output all the odd numbers between 1 and 50, except for the number the user entered.
+
+odd_input = input('please enter an odd number between 1 and 50. ')
+
+while number in odd_input:
+    print('this is an invalid input')
+    if int(number) % 2 == 0 or (int(number) > 50 or int(number) < 1):
+        break
+
+# Use a loop and the continue statement to output all the odd numbers between 1 and 50, except for the number the user entered.
+
+other_odd_input = int(input('please enter an odd number between 1 and 50. '))
+
+for n in range(0, 51):
+    if n != other_odd_input and n % 2 == 0:
+        continue
+    print('Here is an odd number: {}'.format(n))
+
